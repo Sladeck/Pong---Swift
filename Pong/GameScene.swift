79b661062303e627fa7f522c677f5fd253ccce53
SUP'Internet 09 <supinternet09@MacBook-Air-de-SUPInternet-9.local>
2018-03-08 16:58:01 +0100

    [ADD] touch moves

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -17,6 +17,11 @@ class GameScene: SKScene {
     var elements = [String: SKSpriteNode]()
     
     override func didMove(to view: SKView) {
+        
+        elements["player_top"] = self.childNode(withName: "player_top") as! SKSpriteNode
+        
+        elements["player_bottom"] = self.childNode(withName: "player_bottom") as! SKSpriteNode
+        
         elements["ball"] = self.childNode(withName: "ball") as! SKSpriteNode
         
         elements["ball"]?.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
@@ -28,8 +33,37 @@ class GameScene: SKScene {
         self.physicsBody = frameBody
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            
+            if(location.y < 0) {
+            elements["player_bottom"]?.run( SKAction.moveTo(x: location.x, duration: 0.3))
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            
+            if(location.y < 0) {
+                elements["player_bottom"]?.run( SKAction.moveTo(x: location.x, duration: 0.3))
+            }
+            
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        elements["player_top"]?.run( SKAction.moveTo(x: (elements["ball"]?.position.x)!, duration: 0.3))
+        
+        let locationY = elements["ball"]?.position.y
+        
+        if(locationY! > CGFloat(600) || locationY! < CGFloat(-600)) {
+            elements["ball"]?.position = CGPoint(x: 0, y: 0)
+        }
 
     }
 }
